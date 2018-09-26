41bfe243d1ecb31d3d7ffe0811d53342ee68fd8f
daukadolt <daulet.amirkhanov.official@gmail.com>
2018-09-26 20:42:08 +0600

    icpc.cpp: multiple file generations -> single file with passed-as-flags optional libraries or <iostream> by default

--- icpc.cpp
+++ icpc.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
 
+string flags, temp, filename;
 int main(int argc, char *argv[]) {
     for(int i = 1; i<argc; i++) {
-        ofstream file(string(argv[i])+".cpp");
-        file << ""
-        "#include <iostream>\n"
-        "\n"
-        "using namespace std;\n\n"
-        "int main() {\n"
-        "    return 0;\n"
-        "}\n"
-        "" << endl;
-        file.close();
+        if(string(argv[i])[0] == '-') {
+            temp = argv[i];
+            flags += "#include <" + temp.substr(1, temp.size()-1) + ">\n";
+        }
+        else {filename = argv[i];}
     }
+    if(filename.size() == 0) {return 0;}
+    if(flags.size() == 0) {flags = "#include <iostream>\n";}
+    
+    ofstream file( (filename.substr(filename.size()-4, filename.size()-1) == ".cpp") ? filename : filename + ".cpp");
+    file << ""
+    "" + flags + ""
+    "\n"
+    "using namespace std;\n\n"
+    "int main() {\n"
+    "    return 0;\n"
+    "}\n"
+    "" << endl;
+    file.close();
     return 0;
 }
